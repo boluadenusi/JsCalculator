fe2964b5d4126f5ac87183cfd56dcb22e624eef9
Bolu Adenusi <boluadenusi09@gmail.com>
2022-06-22 12:41:10 +0100

    First push to Github for Calculator

---
+++ Calc.js
@@ -0,0 +1,31 @@
+let calculatorAlert = alert ("Welcome to Js Calculator");
+let calculatorPrompt = prompt ('What operation would you like to perform today? "+,-,/,*"');
+
+if (calculatorPrompt === "+"){
+let calculatorPrompt1 = parseInt(prompt ('Enter first number'));
+let calculatorPrompt2 = parseInt(prompt ('Enter second number'));
+let result = alert (calculatorPrompt1 + calculatorPrompt2)
+}
+else if(calculatorPrompt === "-"){
+    let calculatorPrompt1 = parseInt(prompt ('Enter first number'));
+    let calculatorPrompt2 = parseInt(prompt ('Enter second number'));
+    let result = alert (calculatorPrompt1 - calculatorPrompt2)
+    }
+    else if(calculatorPrompt === "*"){
+        let calculatorPrompt1 = parseInt(prompt ('Enter first number'));
+        let calculatorPrompt2 = parseInt(prompt ('Enter second number'));
+        let result = alert (calculatorPrompt1 * calculatorPrompt2)
+        }
+        else if(calculatorPrompt === "/"){
+            let calculatorPrompt1 = parseInt(prompt ('Enter first number'));
+            let calculatorPrompt2 = parseInt(prompt ('Enter second number'));
+            let result = alert (calculatorPrompt1 / calculatorPrompt2)
+            }
+
+
+
+
+
+
+
+    
